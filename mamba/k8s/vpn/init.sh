09193c95146fe301d3cae0d3286a291a4e7c3110
harisato <pp.quochai@gmail.com>
2021-01-06 10:34:11 +0000

    Update helm charts repo

--- mamba/k8s/vpn/init.sh
+++ mamba/k8s/vpn/init.sh
@@ -3,5 +3,5 @@ set -xe
 
 helm repo add stable http://storage.googleapis.com/kubernetes-charts
 
-helm install stable/openvpn \
+helm install ~/.akachain/helm-charts/stable/openvpn \
     --name openvpn
