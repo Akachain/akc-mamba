ff62c5585be28c65ee3f32a89e0e497131075158
Nguyen Quoc Hai <hainq6@fsoft.com.vn>
2021-01-18 09:33:07 +0700

    Fix bug when bootstrap network

--- mamba/setup.py
+++ mamba/setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', 'r') as f:
 
 setuptools.setup(
     name='akc-mamba',
-    version='2.1.1a0',
+    version='2.1.1',
     entry_points={'console_scripts': ['mamba = cli.mamba:mamba']},
     author="akaChain",
     author_email="admin@akachain.io",
